e5ce0717005bd853f684e295fbd24dde345b479e
Min Le <lemin9538@gmail.com>
2021-10-07 11:37:00 +0800

    libc: add code to get HW resource from chiyou
    Signed-off-by: Min Le <lemin9538@gmail.com>

---
+++ user.libc/include/minos/device.h
@@ -0,0 +1,54 @@
+#ifndef __LIBC_DEVICE_H__
+#define __LIBC_DEVICE_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include <minos/types.h>
+
+#define __isb()		asm volatile("isb" : : : "memory")
+#define __dmb(opt)	asm volatile("dmb " #opt : : : "memory")
+#define __dsb(opt)	asm volatile("dsb " #opt : : : "memory")
+
+#define isb()		__isb();
+
+#define mb()		__dsb(sy)
+#define rmb()		__dsb(ld)
+#define wmb()		__dsb(st)
+
+#define dma_rmb()	__dmb(oshld)
+#define dma_wmb()	__dmb(oshst)
+
+#define iormb()		dma_rmb()
+#define iowmb()		dma_wmb()
+
+#define smp_mb()	__dmb(ish)
+#define smp_rmb()	__dmb(ishld)
+#define smp_wmb()	__dmb(ishst)
+
+#define WRITE32(_reg, _val)                                                    \
+	do {                                                                   \
+		register uint32_t __myval__ = (_val);                          \
+		*(volatile uint32_t *)&(_reg) = __myval__;                     \
+	} while (0)
+#define WRITE64(_reg, _val)                                                    \
+	do {                                                                   \
+		register uint64_t __myval__ = (_val);                          \
+		*(volatile uint64_t *)&(_reg) = __myval__;                     \
+	} while (0)
+#define READ32(_reg) (*(volatile uint32_t *)&(_reg))
+#define READ64(_reg) (*(volatile uint64_t *)&(_reg))
+
+int request_irq_by_handle(int handle);
+int request_consequent_pma(size_t memsize);
+void *request_mmio_by_handle(int handle);
+int get_device_mmio_handle(const char *comp, int index);
+int get_device_irq_handle(const char *comp, int index);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ user.libc/src/minos/device.c
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2021 Min Le (lemin9538@gmail.com)
+ */
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+#include "stdio_impl.h"
+#include "libc.h"
+#include <minos/proto.h>
+#include <minos/kobject.h>
+#include <minos/types.h>
+
+static int __request_device_resource(int id, const char *comp, int index)
+{
+	struct proto proto;
+
+	if (libc.chiyou_handle <= 0)
+		return -EPERM;
+
+	if (strlen(comp) >= FILENAME_MAX)
+		return -ENAMETOOLONG;
+
+	proto.proto_id = id;
+	proto.devinfo.key = 0;
+	proto.devinfo.index = index;
+
+	return kobject_write(libc.chiyou_handle, &proto,
+			sizeof(struct proto), (void *)comp, strlen(comp), -1);
+}
+
+int request_irq_by_handle(int handle)
+{
+	return kobject_open(handle);
+}
+
+int request_consequent_pma(size_t memsize)
+{
+	return kobject_create_consequent_pma(KR_RWCM, KR_RWM, memsize);
+}
+
+void *request_mmio_by_handle(int handle)
+{
+	if (kobject_open(handle) < 0)
+		return (void *)-1;
+
+	return kobject_mmap(handle);
+}
+
+int get_device_mmio_handle(const char *comp, int index)
+{
+	return __request_device_resource(PROTO_GET_MMIO, comp, index);
+}
+
+int get_device_irq_handle(const char *comp, int index)
+{
+	return __request_device_resource(PROTO_GET_IRQ, comp, index);
+}

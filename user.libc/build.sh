b6a1852f9ce30c95ff64849d33f15d38423ffda6
Min Le <lemin9538@gmail.com>
2022-01-27 15:50:25 +0800

    support build minos2 on the aarch64 host machine
    use below command to build minos2 if the machine is
    aarch64 host:
     # make PLATFORM=qemu_arm64 CROSS_COMPILE= prepare
     # make CROSS_COMPILE= ramdisk
    
    Signed-off-by: Min Le <lemin9538@gmail.com>

--- user.libc/build.sh
+++ user.libc/build.sh
@@ -14,5 +14,14 @@ fi
 
 echo "LIBC INSTALL-DIR:${INSTALL_DIR} ARCH:${LIBC_TAGET_ARCH} CROSS:${LIBC_CROSS_COMPILE}"
 
-./configure CROSS_COMPILE=${LIBC_CROSS_COMPILE} --disable-shared --target=${LIBC_TAGET_ARCH} \
-	--prefix=${INSTALL_DIR} --with-malloc=mallocng
+HOST_ARCH="$(uname -m)"
+
+# if build on aarch64 host, do not set th --target
+if [ "$HOST_ARCH" = "aarch64" ]; then
+	echo "Build on aarch64 host"
+	./configure --disable-shared --prefix=${INSTALL_DIR} --with-malloc=mallocng
+else
+	echo "Build on x86 host"
+	./configure CROSS_COMPILE=${LIBC_CROSS_COMPILE} --disable-shared --target=${LIBC_TAGET_ARCH} \
+		--prefix=${INSTALL_DIR} --with-malloc=mallocng
+fi
